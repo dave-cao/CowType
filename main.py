38441e30f1403896ecdea2018b2faef6ca984d34
David Cao <dacao@protonmail.com>
2023-01-03 20:51:28 -0700

    finished fixing bugs and start timer

--- main.py
+++ main.py
@@ -28,11 +28,14 @@ class Game:
         # Keeping track of time
         self.time = 0
         self.second = 0
+        self.start_timer = False
 
         # user input
-        self.user_text = ""
-        self.new_line = ""
-        self.input_list = []
+        # self.user_text = ""
+        # self.new_line = ""
+        self.input_offset = self.surface.get_height() // 2
+        self.input_x = 100
+        self.input_list = [Input(self.input_x, self.input_offset, self.surface)]
 
     def play(self):
         # plays the game
@@ -41,7 +44,10 @@ class Game:
             # play frame
             self.handle_events()
             self.draw()
-            self.track_time()
+
+            # Start the timer when a key is pressed
+            if self.start_timer:
+                self.track_time()
             if self.continue_game:
                 pass
 
@@ -54,18 +60,64 @@ class Game:
                 self.close_clicked = True
 
             if event.type == pygame.KEYDOWN:
+                self.check_newline()
                 self.handle_keydown(event)
+                # start the timer when a key is pressed
+                if not self.start_timer:
+                    self.start_timer = True
 
     def handle_keydown(self, event):
         if event.key == pygame.K_BACKSPACE:
-            self.user_text = self.user_text[:-1]
+            self.handle_backspace(event)
 
         else:
-            if self.continue_game:
-                self.user_text += event.unicode
+            self.input_list[-1].update_content(event.unicode)
 
-            if not self.continue_game:
-                self.new_line += event.unicode
+    def handle_backspace(self, event):
+        # handles the backspace event
+        if self.input_list[0].get_content() != " ":
+            self.input_list[-1].delete_char()
+        if not self.input_list[-1].get_content() and self.input_list[0].get_content():
+            self.input_list.pop()
+
+    def check_newline(self):
+        end_of_type_screen = 200
+        for user_input in self.input_list:
+            if (
+                user_input.get_width() > self.surface.get_width() - end_of_type_screen
+                and user_input.check_on()
+            ):
+                self.input_offset += 80
+                self.input_list.append(
+                    Input(self.input_x, self.input_offset, self.surface)
+                )
+
+                self.update_lines(user_input)
+
+    def update_lines(self, user_input):
+        # updates the new lines with the word of the previous line and
+        # updates the previous line without the word of the next line
+
+        # get the last word of current input and give to next input
+        # update the content of the most recent input with the last word
+        # of previous input (current)
+        current_last_word = user_input.get_content().split()[-1]
+        without_last_word = " ".join(user_input.get_content().split()[:-1])
+        # set the current userinput to delete the last word
+        original_input = user_input.get_content()
+        user_input.set_content(without_last_word)
+
+        # if the user_input is blank, then don't do this
+        if user_input.get_content():
+            self.input_list[-1].update_content(current_last_word)
+
+            # Makes it so we do not update the old lines
+            user_input.switch_on_to_off()
+
+        else:
+            # if the user content is blank
+            user_input.set_content(original_input)
+            user_input.switch_on_to_off()
 
     def track_time(self):
         self.time += 1
@@ -80,9 +132,12 @@ class Game:
         # if self.second < 2:
         #    self.draw_intro()
 
-        self.draw_inputbox()
-        if not self.continue_game:
-            self.draw_new_line()
+        # self.draw_inputbox()
+        # if not self.continue_game:
+        #     self.draw_new_line()
+
+        for user_input in self.input_list:
+            user_input.draw_content()
 
         pygame.display.update()
 
@@ -93,58 +148,61 @@ class Game:
         image = pygame.transform.scale(image, (image_width, image_height))
         self.surface.blit(image, (0, 0))
 
-    def draw_inputbox(self):
 
-        font = pygame.font.Font("./font/Andika-Regular.ttf", 30)
-        input_rect = pygame.Rect(200, 200, 30, 50)
-        color_active = pygame.Color("#665c54")
-        color_passive = pygame.Color("#ebdbb2")
-        color = pygame.Color("white")
+class Input:
+    def __init__(self, x, y, surface):
+        width = 30
+        height = 50
+        self.x = x
+        self.y = y
 
-        x = 100
-        y = self.surface.get_height() - 200
-        input_rect.x = x
-        input_rect.y = y
+        self.font = pygame.font.Font("./font/Andika-Regular.ttf", 30)
+        self.rect = pygame.Rect(x, y, width, height)
 
-        text_box = font.render(self.user_text, True, color_active)
+        self.surface = surface
+        self.color_active = pygame.Color("#665c54")
+        self.color_passive = pygame.Color("#ebdbb2")
+        self.bg_color = pygame.Color("#262626")
 
-        location = input_rect.x + 5, input_rect.y
+        self.content = ""
 
-        input_rect.w = max(input_rect.w, text_box.get_width() + 10)
-        pygame.draw.rect(self.surface, color_passive, input_rect)
+        self.on = True
 
-        if text_box.get_width() >= self.surface.get_width() - 200:
-            if self.continue_game:
-                self.new_line += self.user_text.split()[-1]
-                new_user = self.user_text.split()[:-1]
-                self.user_text = " ".join(new_user)
-            self.continue_game = False
+    def draw_content(self):
+
+        # input
+
+        # blit the text box
+        text_box = self.font.render(self.content, True, self.color_active)
+        location = self.rect.x + 5, self.rect.y
+
+        # draw the rect
+        self.rect.width = text_box.get_width() + 10
+        pygame.draw.rect(self.surface, self.color_passive, self.rect)
+
+        # blit the text after so it shows up first
         self.surface.blit(text_box, location)
 
-        # When the text box width reaches a certain width, then we
-        # we have to go to a new line beneath it.
-        # create another textbox, with it's user text being split
-        # The continue on with a different position
+    def update_content(self, letter):
+        self.content += letter
 
-    def draw_new_line(self):
-        font = pygame.font.Font("./font/Andika-Regular.ttf", 30)
-        input_rect = pygame.Rect(200, 200, 30, 50)
-        color_active = pygame.Color("#665c54")
-        color_passive = pygame.Color("#ebdbb2")
-        color = pygame.Color("white")
+    def delete_char(self):
+        self.content = self.content[:-1]
 
-        x = 100
-        y = self.surface.get_height() - 140
-        input_rect.x = x
-        input_rect.y = y
+    def get_width(self):
+        return self.rect.width
 
-        text_box = font.render(self.new_line, True, color_active)
+    def check_on(self):
+        return self.on
 
-        location = input_rect.x + 5, input_rect.y
+    def switch_on_to_off(self):
+        self.on = False
 
-        input_rect.w = max(input_rect.w, text_box.get_width() + 10)
-        pygame.draw.rect(self.surface, color_passive, input_rect)
-        self.surface.blit(text_box, location)
+    def get_content(self):
+        return self.content
+
+    def set_content(self, content):
+        self.content = content
 
 
 if __name__ == "__main__":
